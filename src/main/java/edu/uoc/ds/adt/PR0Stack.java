42d949f5a599fb7e6460131696487e4fb1375025
Juan R. Gavilanes <jrgavilanes@gmail.com>
2022-09-29 18:50:02 +0200

    first

---
+++ src/main/java/edu/uoc/ds/adt/PR0Stack.java
@@ -0,0 +1,39 @@
+package edu.uoc.ds.adt;
+
+
+import edu.uoc.ds.adt.sequential.Stack;
+import edu.uoc.ds.adt.sequential.StackArrayImpl;
+
+public class PR0Stack {
+
+    // Capacitat màxima de la seqüència.
+    public final int CAPACITY = 9;
+
+    private Stack<Integer> stack;
+
+    public PR0Stack() {
+        newStack();
+    }
+
+    public void newStack() {
+        stack = new StackArrayImpl<Integer>(CAPACITY);
+    }
+
+
+    public void fillStack() {
+        for (int c = 0; c < 9; c++) {
+            stack.push(c);
+        }
+    }
+
+    public String clearAllStack() {
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty())
+            sb.append(stack.pop()).append(" ");
+        return sb.toString();
+    }
+
+    public Stack<Integer> getStack() {
+        return this.stack;
+    }
+}
